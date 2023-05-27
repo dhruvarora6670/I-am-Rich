4b5247056ad37d397a7d165876be8994973bd497
dhruv6670 <68185043+dhruv6670@users.noreply.github.com>
2023-05-27 21:51:12 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dhruv Arora on 27/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
